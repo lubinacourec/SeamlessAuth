8fa6b32637941c902827e633885312c69f01b154
Anonymous <nobody@example.com>
2023-12-03 13:19:10 +0800

    Add acceptable remote versions value

--- src/main/java/anon/seamlessauth/SeamlessAuth.java
+++ src/main/java/anon/seamlessauth/SeamlessAuth.java
@@ -10,7 +10,7 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 
-@Mod(modid = Tags.MODID, version = Tags.VERSION, name = Tags.MODNAME, acceptedMinecraftVersions = "[1.7.10]")
+@Mod(modid = Tags.MODID, version = Tags.VERSION, name = Tags.MODNAME, acceptedMinecraftVersions = "[1.7.10]", acceptableRemoteVersions = "[1.2.0,)")
 public class SeamlessAuth {
 
     public static final Logger LOG = LogManager.getLogger(Tags.MODID);
